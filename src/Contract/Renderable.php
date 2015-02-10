c05cb86716f30a2215526d23f1faf774d12c72b2
hbc <bcxxxxxx@gmail.com>
2015-02-10 22:02:18 +0800

    Use illuminate's contract.

--- src/Contract/Renderable.php
+++ src/Contract/Renderable.php
@@ -1,9 +1,13 @@
 <?php namespace Flamingo\Fluent\Contract;
 
-interface Renderable {
+use Illuminate\Contracts\Support\Renderable as IlluminateRenderable;
+
+interface Renderable extends IlluminateRenderable {
 
     /**
      * Get the rendered content of the object.
+     *
+     * @return string
      */
     public function render();
 
